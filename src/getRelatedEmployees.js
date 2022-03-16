9a284dd1a37be515424f082a3d700852d8a35b71
Amanda Perch <perch_amanda@hotmail.com>
2022-03-16 17:41:43 -0300

    Adiciona primeira parte da questao 4) Implemente a função getRelatedEmployees 1- isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,8 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((gerentes) => gerentes.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
