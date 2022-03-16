eba551a3ef3cccf3bad6524bbf1666cd1b625dec
Amanda Perch <perch_amanda@hotmail.com>
2022-03-16 16:38:04 -0300

    Adiciona questao 1) Implemente a função getSpeciesByIds usando map e find

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
-
 module.exports = getSpeciesByIds;
