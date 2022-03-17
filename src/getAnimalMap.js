b2797982540ae55d59ea1e0b132e684257ac0678
Amanda Perch <perch_amanda@hotmail.com>
2022-03-17 18:12:54 -0300

    Adiciona tentativa da questao 7)Implemente a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,37 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const localizacaoAnimal = () => {
+  const objeto = {};
+  species.forEach(({ location, name }) => {
+    if (!objeto[location]) {
+      objeto[location] = [];
+    }
+    objeto[location].push(name);
+  });
+  return objeto;
+};
+
+const filtro = (sorted, sex) => {
+  const objeto = {};
+  species.forEach(({ location, name, residents }) => {
+    if (!objeto[location]) {
+      objeto[location] = [];
+    }
+    let mapa = [...residents];
+    if (sex) {
+      mapa = mapa.filter((genero) => genero.sex === sex);
+    }
+    mapa = mapa.map((resident) => resident.nome);
+    if (sorted) mapa.sort();
+    objeto[location].push({ [name]: mapa });
+  });
+  return objeto;
+};
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) return filtro(sorted, sex);
+  return localizacaoAnimal();
 }
 
 module.exports = getAnimalMap;
