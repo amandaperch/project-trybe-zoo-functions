009284435bcc35dea15b9813e25ab97317ac579d
Amanda Perch <perch_amanda@hotmail.com>
2022-03-17 16:39:45 -0300

    Adiciona primeira parte da questao 6)  Implemente a função calculateEntry - countEntrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const criancas = entrants.filter((entrant) => entrant.age < 18).length;
+  const adulto = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const idosos = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child: criancas, adult: adulto, senior: idosos };
 }
 
 function calculateEntry(entrants) {
