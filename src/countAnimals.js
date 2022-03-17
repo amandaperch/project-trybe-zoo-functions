a6162b189babb7fb1678b6d8b5e8bbd394fdf28f
Amanda Perch <perch_amanda@hotmail.com>
2022-03-17 14:51:56 -0300

    Adiciona questao 5) Implemente a função countAnimals, apos muitas tentativas passou no npm

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const contador = () => species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
+
+const buscador = (animal) => species
+  .find((specie) => specie.name === (animal.specie)).residents;
+
+const sex = (animal) => buscador(animal)
+  .filter((residents) => residents.sex === (animal.sex));
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return contador();
+  }
+  return animal.sex
+    ? sex(animal).length
+    : buscador(animal).length;
 }
 
 module.exports = countAnimals;
